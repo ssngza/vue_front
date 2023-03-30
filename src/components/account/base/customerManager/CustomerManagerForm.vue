c27228023ebb4d10f50e29a1a1725fb6aece7bc1
guswn <guswns759@naver.com>
2023-03-30 21:26:28 +0900

    2023-03-30 회계 거래처관리 라우트생성(조현준)

---
+++ src/components/account/base/customerManager/CustomerManagerForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+  <CommonGrid
+    title="거래처코드"
+    grid-type="grid"
+    :buttonlist="buttonList"
+  />
+  </div>
+</template>
+
+<script>
+import CommonGrid from '@/components/logistic/sales/gridComponent/CommonGrid.vue'
+
+export default {
+  components:{
+    CommonGrid
+
+  },
+  data: ()=>({
+    buttonList: [
+      {
+        value:'조회',
+        event:'search',
+        method:''
+      },
+      {
+        value:'추가',
+        event: 'add',
+      },
+      {
+        value:'삭제',
+        event:'delete',
+      },
+      {
+        value:'저장',
+        event:'save'
+      },
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
