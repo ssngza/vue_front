b3b23ca2226b853eb76168155fa4f6bd3725fe7d
김승현 <ksh534700@gmail.com>
2023-03-30 17:35:04 +0900

    23.3.30 로그인부분 수정(김승현)

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,6 +1,6 @@
 import { $themeConfig } from '@themeConfig'
 import axios from 'axios'
-import { hrApi } from '@/api'
+import {hrApi, sys} from '@/api'
 
 export default {
   namespaced: true,
@@ -46,7 +46,7 @@ export default {
       const {
         companyCode, workplaceCode, empCode, password,
       } = payload
-      const { data } = await axios.post('http://localhost:4000/erp/sys/login', {
+      const { data } = await sys.post('/login', {
         empCode,
         companyCode,
         workplaceCode,
