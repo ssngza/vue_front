18d02f51247ddf9beaa1846d5007018cc595f9ee
siyun <siyun0982@gmail.com>
2023-03-31 21:04:47 +0900

    23.3.31 자재명세서(BOM) 정전개/역전개 구현중

---
+++ src/views/logistic/purchase/LogisticsBOM/LogisticsBOM.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <component :is="'b-card'">
+
+    <b-tabs pills>
+      <b-tab
+          active
+      >
+        <template #title>
+          <feather-icon
+              icon="CornerDownRightIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+          />
+          <span class="d-none d-sm-inline">BOM 정전개/역전개</span>
+        </template>
+        <!--  내용물 컴포넌트       -->
+        <Deploy />
+      </b-tab>
+
+      <b-tab >
+        <template #title>
+          <feather-icon
+              icon="CornerDownRightIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+          />
+          <span class="d-none d-sm-inline">BOM 등록/수정</span>
+        </template>
+        <!--  내용물 컴포넌트       -->
+        <Regist/>
+      </b-tab>
+
+    </b-tabs>
+  </component>
+
+</template>
+
+<script>
+
+import {
+  BTab, BTabs, BCard,
+} from 'bootstrap-vue'
+
+import Deploy from "@/views/logistic/purchase/LogisticsBOM/Deploy/Deploy";
+import Regist from "@/views/logistic/purchase/LogisticsBOM/Regist/Regist";
+
+export default {
+  components: {
+    Regist,
+    Deploy,
+    BTab,
+    BTabs,
+    BCard,
+  },
+}
+
+</script>
+
+<style  lang="scss">
+@import '@core/scss/vue/libs/vue-flatpicker.scss';
+</style>
---
+++ src/views/logistic/purchase/LogisticsBOM/Regist/Regist.vue
@@ -0,0 +1,130 @@
+<template>
+
+  <div>
+    <div  style="margin: 30px 0 0 -14px; width: 550px; display: flex">
+
+      <b-button-group style="height: 80%">
+        <b-button
+            v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+            variant="outline-primary"
+        >
+          검색
+        </b-button>
+        <!--        이벤트걸기-->
+        <b-button
+            v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+            variant="outline-primary"
+        >
+          BOM조회
+        </b-button>
+      </b-button-group>
+
+    </div>
+
+    <div  style="margin: 30px 0 0 -14px; width: 550px; display: flex">
+
+      <b-button-group style="height: 80%">
+        <b-button
+            v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+            variant="outline-primary"
+        >
+          새로운 항목 추가
+        </b-button>
+
+        <b-button
+            v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+            variant="outline-primary"
+        >
+          삭제
+        </b-button>
+
+        <b-button
+            v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+            variant="outline-primary"
+        >
+          일괄저장
+        </b-button>
+      </b-button-group>
+
+    </div>
+
+    <b-table
+        style="max-height: 300px; overflow: auto; width: 100%"
+        responsive
+        :items="estimateDetailList"
+        :fields="fields"
+        class="mb-0 scrollStyle"
+    />
+    <!--    수정하기-->
+
+  </div>
+</template>
+
+<script>
+import {BButton, BButtonGroup, BTable} from 'bootstrap-vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    BTable,
+    BButton,
+    BButtonGroup,
+  },
+  computed: {
+    ...mapState('logi/sales', ['estimateDetailList']),
+  },
+  /*  수정하기*/
+  data() {
+    return {
+      fields: [
+        {
+          key: 'NO',
+          label: 'NO',
+          sortable: 'true',
+        },
+        {
+          key: 'itemClassificationName',
+          label: '품목구분',
+        },
+        {
+          key: 'itemCode',
+          label: '품목코드',
+        },
+        {
+          key: 'itemName',
+          label: '품목명',
+        },
+        {
+          key: 'netAmount',
+          label: '정미수량',
+        },
+        {
+          key: 'description',
+          label: '설명',
+        },
+        {
+          key: 'status',
+          label: 'status',
+        },
+        {
+          key: 'itemClassification',
+          label: 'itemClassification',
+        },
+        {
+          key: 'parentItemCode',
+          label: 'parentItemCode',
+        },
+        {
+          key: 'beforeStatus',
+          label: 'beforeStatus',
+        },
+        {
+          key: 'deleteStatus',
+          label: 'deleteStatus',
+        },
+      ],
+    }
+  },
+}
+
+</script>
