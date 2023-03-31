03d43e4e8492807e2e78c24885bfa0f24c2054c5
이유정 <pp89925244@gmail.com>
2023-03-31 16:40:20 +0900

    탭 3개의 그리드 완성

---
+++ src/components/logistic/production/workInstruction/WorkPerformanceManagement.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <b-card
+      no-body
+      style="font-family: '배달의민족 도현'; max-height: 500px"
+  >
+    <div class="m-2">
+
+      <h2 style="margin-bottom: 30px">생산실적관리</h2>
+      <b-button
+          v-ripple.400="'rgba(113, 102, 240, 0.15)'"
+          variant="outline-primary"
+      >
+        생산실적조회
+      </b-button>
+
+      <div style="margin-top: 30px">
+        <b-table
+            class="editable-table"
+            hover
+            selectable
+            show-empty
+            empty-text="No matching records found"
+            :select-mode="'single'"
+            :fields="workOrderStatusList"
+        />
+      </div>
+
+    </div>
+
+  </b-card>
+</template>
+
+<script>
+import { BCard, BCardText, BLink } from 'bootstrap-vue'
+import {workOrderStatusList} from "@/components/logistic/production/fields";
+
+export default {
+  components: {
+    BCard,
+    BCardText,
+    BLink,
+  },
+  data: () => ({
+    selected: '',
+    selectMode: 'single',
+    modal: false,
+    workOrderStatusList
+  }),
+}
+</script>
+
+<style  lang="scss">
+@import '@core/scss/vue/libs/vue-flatpicker.scss';
+</style>
